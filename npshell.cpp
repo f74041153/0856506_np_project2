f758f07acf33fce5926152660db4db8dc1cb40b5
rslai <rslai@nplinux12.cs.nctu.edu.tw>
2019-10-19 13:26:46 +0800

    pipe without stderr and stream

--- npshell.cpp
+++ npshell.cpp
@@ -80,34 +80,38 @@ bool fileExist(string filename){
 	return (stat(filename.c_str(),&buf) == 0);
 }
 
-pid_t create_process(string cmd_path,vector<string> arg,int pipe_in,int pipe_out){
-	// process execv arg
+pid_t create_process(string cmd_path,vector<string> arg,int cmd_std_in,int cmd_std_out,vector<struct Pipe>& pipe_table){
+	// rearrange execv arg
 	vector<char*>argv;
 	for(int i=0;i<arg.size();i++){
 		argv.push_back((char*)arg[i].c_str());
 	}
-	argv.push_back(NULL);
-	
+	argv.push_back(NULL);	
 	// fork process
-	pid_t pid = fork();
+	pid_t pid;
+       	while((pid = fork())<0){
+		usleep(1000);
+	}
+
 	if(pid < 0){
+		cout << "fail" << endl;
 		exit(EXIT_FAILURE);
-	}else if (pid == 0){
-		// child
-		if(pipe_in != STDIN_FILENO){
-			dup2(pipe_in,STDIN_FILENO);
-			close(pipe_in);
+	}else if (pid == 0){ // child 		
+		if(cmd_std_in != STDIN_FILENO){
+			dup2(cmd_std_in,STDIN_FILENO);
 		}
-		if (pipe_out != STDOUT_FILENO){
-			dup2(pipe_out,STDOUT_FILENO);
-			close(pipe_out);
+		if (cmd_std_out != STDOUT_FILENO){
+			dup2(cmd_std_out,STDOUT_FILENO);
+		}
+
+		for(int i=0;i<pipe_table.size();i++){
+			close(pipe_table[i].pipe_in);
+			close(pipe_table[i].pipe_out);
 		}	
-				
 		if(execv(cmd_path.c_str(), &argv[0]) < 0){
 			exit(EXIT_FAILURE);
 		}
-	}else{
-		// parent
+	}else{ // parent
 		return pid;
 	}
 }
@@ -130,24 +134,19 @@ int search_pipe(vector<struct Pipe>& pipe_table,int n){
 void create_new_pipe(vector<struct Pipe>& pipe_table,int fd[],int N){
 	pipe(fd);
 	struct Pipe new_pipe;
-	new_pipe.pipe_in = fd[0];
-	new_pipe.pipe_out = fd[1];
+	new_pipe.pipe_in = fd[1];
+	new_pipe.pipe_out = fd[0];
 	new_pipe.cmd_cntdwn = N;
 	pipe_table.push_back(new_pipe);
 }
 
 int main(){
-
 	/* default path */
-	my_setenv("PATH","bin:.");
-
+	my_setenv("PATH","bin:.");	
 	vector<struct Pipe> pipe_table;
+	string line;
 	cout << "% ";
-	while(1){
-		
-		string line;
-		getline(cin,line);
-
+	while(getline(cin,line)){
 		/* handle input*/
 		vector<struct CMD> cmds = parse_cmd(line);
 		
@@ -160,60 +159,59 @@ int main(){
 			my_exit();
 		}
 		
-		string exe_path = "bin/" + cmds[0].parsed_cmd[0];
-		if(fileExist(exe_path)){
+		/* other cmd */	
+		pid_t last_cmd_pid;
+		signal(SIGCHLD,childHandler);	
+		for(int i=0;i<cmds.size();i++){
 			
-			pid_t last_cmd_pid;
-			signal(SIGCHLD,childHandler);
-			
-			for(int i=0;i<cmds.size();i++){
+			string exe_path = "bin/" + cmds[i].parsed_cmd[0];
+			if(!fileExist(exe_path))continue;
 				
-				int cmd_std_in,cmd_std_out,p,fd[2];
-				update_pipe_table(pipe_table);
+			int cmd_std_in,cmd_std_out,p,fd[2];
+			update_pipe_table(pipe_table);
 				
-				// stdin for this cmd
-				p = search_pipe(pipe_table,0);
-				if(p>=0) cmd_std_in = pipe_table[p].pipe_out;
-				else cmd_std_in = STDIN_FILENO;
-
-				// stdout for this cmd 
-				if(cmds[i].N>0){
-					// pipeN
-					p = search_pipe(pipe_table,cmds[i].N);
-					if(p<0){
-						// create new pipe
-						create_new_pipe(pipe_table,fd,cmds[i].N);
-						cmd_std_out = fd[0];
-					}else{
-						cmd_std_out = pipe_table[p].pipe_in;		
-					}				
-				}else if(i<cmds.size()-1){
-					// pipe
-				//	cout << 1 << endl;
-					p = search_pipe(pipe_table,1);
-					if(p<0){
-						create_new_pipe(pipe_table,fd,1);
-						cmd_std_out = fd[0];
-					}else{
-						cmd_std_out = pipe_table[p].pipe_in;	
-					}
-				}else{
-				//	cout << 0 << endl;
-					cmd_std_out = STDOUT_FILENO; 
-				}
-				cout << STDIN_FILENO << "\t" << cmd_std_in << endl;
-				cout << STDOUT_FILENO << "\t"<<cmd_std_out << endl;
-				
-				last_cmd_pid = create_process(exe_path,cmds[0].parsed_cmd,cmd_std_in,cmd_std_out);
-				if(fd[0]!=STDIN_FILENO) close(fd[0]);
-				if(fd[1]!=STDOUT_FILENO) close(fd[1]);			
+			// stdin for this cmd
+			p = search_pipe(pipe_table,0);
+			int pipe_of_this_cmd = p;
+			if(p>=0){
+				cmd_std_in = pipe_table[p].pipe_out;
+			}else {
+				cmd_std_in = STDIN_FILENO;
 			}
-			int status;
-			waitpid(last_cmd_pid,&status,0);
-			cout << "success!" << endl;		
-		}
-		cout << "% ";
 
+			// stdout for this cmd 
+			if(cmds[i].N>0){
+				// pipeN
+				p = search_pipe(pipe_table,cmds[i].N);
+				if(p<0){
+					create_new_pipe(pipe_table,fd,cmds[i].N);
+					cmd_std_out = fd[1]; 
+				}else{
+					cmd_std_out = pipe_table[p].pipe_in;		
+				}				
+			}else if(i<cmds.size()-1){
+				// pipe
+				p = search_pipe(pipe_table,1);
+				if(p<0){
+					create_new_pipe(pipe_table,fd,1);
+					cmd_std_out = fd[1];
+				}else{
+					cmd_std_out = pipe_table[p].pipe_in;	
+				}
+			}else{
+				cmd_std_out = STDOUT_FILENO; 
+			}
+			last_cmd_pid = create_process(exe_path,cmds[i].parsed_cmd,cmd_std_in,cmd_std_out,pipe_table);
+					
+			if(pipe_of_this_cmd >= 0) {
+				close(pipe_table[pipe_of_this_cmd].pipe_in);
+				close(pipe_table[pipe_of_this_cmd].pipe_out);
+				pipe_table.erase(pipe_table.begin()+pipe_of_this_cmd);
+			}			
+		}
+		int status;
+		waitpid(last_cmd_pid,&status,0);
+		cout << "% ";
 	}	
 	return 0;
 }
